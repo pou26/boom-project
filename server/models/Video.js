c06d437c0acf7f65a8c3325b0245a8b36038fe95
pou26 <poushaliaich1999@gmail.com>
2025-05-22 21:51:42 +0530

    Commit

--- server/models/Video.js
+++ server/models/Video.js
@@ -6,6 +6,7 @@ const VideoSchema = new mongoose.Schema({
   videoPath: String,
   thumbnailPath: String,
   likes: { type: Number, default: 0 },
+  
 });
 
 module.exports = mongoose.model('Video', VideoSchema);
